c24da498b9413bb6f8d8101159f0d8edcc85ffd4
kuntalbanik <deisiduke1986@gmail.com>
2025-01-18 14:59:01 +0530

    views & url update

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -27,10 +29,13 @@ monthly_challenges = {
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
+
     if month > len(months):
         return HttpResponseNotFound("This month is not supported!")
+
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])  # /challenge/june
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):

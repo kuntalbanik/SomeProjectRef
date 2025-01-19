ffd5ab1a9598b20519f1b78503b4d15b6a253bcd
Kuntal Banik <deisiduke1986@gmail.com>
2025-01-19 21:37:04 +0530

    update views

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # path("january", views.january),
     # path("february", views.february),
+    path("", views.index),  # /challenges/
     # Dynamic path segment. Placeholder segment.
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),

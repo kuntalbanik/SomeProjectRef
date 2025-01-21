787493f728c285c2b855daad3e3fc1107854555a
kuntalbanik <deisiduke1986@gmail.com>
2025-01-21 15:25:33 +0530

    update rename file

--- Codes/rename_file.py
+++ Codes/rename_file.py
@@ -1,13 +1,34 @@
 import os
+
+def main(start_path='.'):
+	for root, dirs, files in os.walk(start_path):
+		for file in files:
+			filename = os.path.join(root, file)
+			if filename.endswith("_en.srt"):
+				# print(filename)
+				new_name = filename[:-7] + ".srt"
+				os.rename(filename, new_name)
+	
+
+if __name__ == '__main__':
+	# Calling main() function
+	# Specify the directory path you want to start from
+	directory_path = './'
+	main(directory_path)
+
+
+
+
+# Old code
 # Function to rename multiple files
-def main():
-	i = 0
-	path="."
-	for filename in os.listdir(path):
-		# print(filename)
-		if filename.endswith("_en.srt"):
-			new_name = filename[:-7] + ".srt"
-			os.rename(filename, new_name)
+# def main():
+#	i = 0
+#	path="."
+#	for filename in os.listdir(path):
+		# # # print(filename)
+#		if filename.endswith("_en.srt"):
+#			new_name = filename[:-7] + ".srt"
+#			os.rename(filename, new_name)
 		# my_dest ="new" + str(i) + ".srt"
 		# print(my_dest)
 		# my_source =path + filename
@@ -17,6 +38,6 @@ def main():
 		# os.rename(my_source, my_dest)
 		# i += 1
 # Driver Code
-if __name__ == '__main__':
+# if __name__ == '__main__':
 	# Calling main() function
-	main()
+	# main()

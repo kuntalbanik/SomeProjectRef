a31d80614798714f4708b87c52be5649fa7cd3e7
kuntalbanik <deisiduke1986@gmail.com>
2025-01-25 15:48:27 +0530

    class with private variable

--- Codes/practice_codes.py
+++ Codes/practice_codes.py
@@ -1,1 +1,18 @@
 # Practice codes
+
+
+# Class example
+class Student:
+    def __init__(self):
+        self.__name = ""
+
+    def getname(self):
+        return self.__name
+
+    def setname(self, name):
+        self.__name = name
+
+
+student_obj = Student()
+student_obj.setname("John")
+print(student_obj.getname())

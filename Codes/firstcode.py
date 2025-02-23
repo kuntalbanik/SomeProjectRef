eb3304a5e7bbe20f0fe3c191f2eb224834a8f6c5
Kuntal Banik <deisiduke1986@gmail.com>
2025-02-23 13:43:30 +0530

    David Bombal codes

--- Codes/firstcode.py
+++ Codes/firstcode.py
@@ -1,2 +1,12 @@
 print("Hello World")
 print("Another print")
+
+fav_color = input("Enter your favorite color : ").lower()
+
+match fav_color:
+    case "green":
+        print(f"your favorite color is {fav_color}")
+    case "black":
+        print(f"your favorite color is {fav_color}")
+    case _:
+        print(f"Your favorite color is not matched with our system")
